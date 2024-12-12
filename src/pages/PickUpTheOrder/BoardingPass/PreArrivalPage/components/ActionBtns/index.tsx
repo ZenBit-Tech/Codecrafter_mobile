d7fa2ba091803a3930ac3656b9f0a729ce3380ca
Parle648 <sashahoumi@gmail.com>
2024-12-12 14:21:41 +0200

    + Add: navigation for ActionBtns

--- src/pages/PickUpTheOrder/BoardingPass/PreArrivalPage/components/ActionBtns/index.tsx
+++ src/pages/PickUpTheOrder/BoardingPass/PreArrivalPage/components/ActionBtns/index.tsx
@@ -1,13 +1,23 @@
 import { Box } from '@mui/system';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { actionBtnsContainer, backButtonStyles, buttonStyles } from './styles';
 
 import Button from '@/components/Button';
 
-export const ActionBtns: FC = () => (
-  <Box sx={actionBtnsContainer}>
-    <Button sx={backButtonStyles} label='Back' variant='outlined' />
-    <Button sx={buttonStyles} label='Transporter Locked' variant='colored' />
-  </Box>
-);
+export const ActionBtns: FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={actionBtnsContainer}>
+      <Button sx={backButtonStyles} label='Back' variant='outlined' />
+      <Button
+        sx={buttonStyles}
+        label='Transporter Locked'
+        variant='colored'
+        onClick={() => navigate('/app/identity-verification')}
+      />
+    </Box>
+  );
+};
