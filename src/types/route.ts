b9d67e6a4c3690fc3d2cb547e2fcaaf4a9c1dbbf
Oleksandr Ptukha <oleksandrptuha1@gmail.com>
2024-12-17 17:21:52 +0200

    fix: fixed route type

--- src/types/route.ts
+++ src/types/route.ts
@@ -37,27 +37,4 @@ export interface Route {
   route_arrival_date: Date;
   route_distance: number;
   route_status: RouteStatuses;
-  route_createdAt: Date;
-  route_updatedAt: Date;
-  route_deletedAt: Date | null;
-  route_userIdId: number;
-  route_companyIdId: number;
-  user_id: number;
-  user_full_name: string;
-  user_email: string;
-  user_role: string;
-  user_phone_number: string;
-  user_otp: string;
-  user_otpExpiry: Date;
-  user_createdAt: Date;
-  user_updatedAt: Date;
-  user_companyIdId: number;
-  company_id: number;
-  company_name: string;
-  company_client_name: string;
-  company_logo: string;
-  company_email: string;
-  company_createdAt: Date;
-  company_updatedAt: string;
-  ordersCount: string;
 }
