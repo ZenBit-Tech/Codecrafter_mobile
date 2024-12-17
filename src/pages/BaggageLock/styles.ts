30e04cb95a478a2619324b3f5a42bc9544b92df1
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-12-17 20:31:59 +0200

    feat(baggageLock): ui for locking the baggage added

---
+++ src/pages/BaggageLock/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from '@mui/system';
+
+export const OrderedList = styled('ol')({
+  listStyleType: 'decimal',
+  paddingLeft: '16px',
+  margin: 0,
+});
+
+export const OrderedListItem = styled('li')({
+  marginBottom: '8px',
+});
