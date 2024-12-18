14a856a88d70d9fbe9477ee83a6cd0f847754d25
Parle648 <sashahoumi@gmail.com>
2024-12-18 14:56:47 +0200

    + Refactoring: CheckBaggagePage

--- src/pages/PickUpTheOrder/BaggageCheck/BaggageCheckPage/components/CheckBaggageBlock/index.tsx
+++ src/pages/PickUpTheOrder/BaggageCheck/BaggageCheckPage/components/CheckBaggageBlock/index.tsx
@@ -2,126 +2,46 @@ import { FC } from 'react';
 
 import { Box, Typography } from '@mui/material';
 
-import { COLORS } from '@/constants/colors';
-import { FONT } from '@/constants/font';
+import {
+  bagDetails,
+  bagTitle,
+  calculatorBlock,
+  checkBaggageBlock,
+  importantBlock,
+  importantText,
+  importantTitle,
+  inputField,
+  weightLabel,
+} from './styles';
+
 import { InputField } from '@/pages/FailedReason/styles';
 
 export const CheckBaggageBlock: FC = () => {
   return (
-    <Box sx={{ marginTop: '87px' }}>
-      <Box
-        sx={{
-          height: '175px',
-          padding: '10px 3px',
-          border: `1px solid ${COLORS.schemes.outlineVariant}`,
-          width: '93vw',
-          margin: '0 auto',
-          borderRadius: '10px',
-          marginBottom: '27px',
-        }}
-      >
-        <Typography
-          sx={{
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.mediumPlus,
-            fontWeight: FONT.fontWeight.large,
-            color: COLORS.schemes.secondary,
-            marginLeft: '10px',
-          }}
-        >
-          Medium bag 1
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.medium,
-            fontWeight: FONT.fontWeight.medium,
-            color: COLORS.schemes.outline,
-            marginLeft: '10px',
-          }}
-        >
-          Max 70 x 50 x 30 cm
-        </Typography>
+    <Box sx={checkBaggageBlock}>
+      <Box sx={calculatorBlock}>
+        <Typography sx={bagTitle}>Medium bag 1</Typography>
+        <Typography sx={bagDetails}>Max 70 x 50 x 30 cm</Typography>
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography
-            sx={{
-              paddingLeft: '10px',
-              fontFamily: FONT.family,
-              fontSize: FONT.fontSize.medium,
-              letterSpacing: '0.2px',
-              color: COLORS.schemes.secondary,
-              marginRight: '5px',
-            }}
-          >
-            Weight
-          </Typography>
+          <Typography sx={weightLabel}>Weight</Typography>
           <InputField
-            sx={{
-              width: '60px',
-              height: '32px',
-              padding: '1px',
-              '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
-                padding: '4px',
-              },
-            }}
+            sx={inputField}
             label=''
             type='number'
             variant='outlined'
           />
         </Box>
       </Box>
-      <Box
-        sx={{
-          height: '175px',
-          padding: '10px 3px',
-          border: `1px solid ${COLORS.schemes.outlineVariant}`,
-          width: '93vw',
-          margin: '0 auto',
-          borderRadius: '10px',
-          marginBottom: '27px',
-        }}
-      >
-        <Typography
-          sx={{
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.mediumPlus,
-            marginLeft: '10px',
-          }}
-        >
-          Important ❗
-        </Typography>
-        <Typography
-          sx={{
-            paddingLeft: '10px',
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.medium,
-            letterSpacing: '0.2px',
-            color: COLORS.schemes.secondary,
-          }}
-        >
+      <Box sx={importantBlock}>
+        <Typography sx={importantTitle}>Important ❗</Typography>
+        <Typography sx={importantText}>
           Count the number of baggage items and weigh them.
         </Typography>
-        <Typography
-          sx={{
-            paddingLeft: '10px',
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.medium,
-            letterSpacing: '0.2px',
-            color: COLORS.schemes.secondary,
-          }}
-        >
+        <Typography sx={importantText}>
           - If the number and weight match the booking details, confirm it.
         </Typography>
-        <Typography
-          sx={{
-            paddingLeft: '10px',
-            fontFamily: FONT.family,
-            fontSize: FONT.fontSize.medium,
-            letterSpacing: '0.2px',
-            color: COLORS.schemes.secondary,
-          }}
-        >
-          - If discrepancies exist, informs the customer to adjust the weight
+        <Typography sx={importantText}>
+          - If discrepancies exist, inform the customer to adjust the weight.
         </Typography>
       </Box>
     </Box>
---
+++ src/pages/PickUpTheOrder/BaggageCheck/BaggageCheckPage/components/CheckBaggageBlock/styles.ts
@@ -0,0 +1,75 @@
+import { SxProps, Theme } from '@mui/system';
+
+import { COLORS } from '@/constants/colors';
+import { FONT } from '@/constants/font';
+
+export const checkBaggageBlock: SxProps<Theme> = { marginTop: '87px' };
+
+export const calculatorBlock: SxProps<Theme> = {
+  height: '175px',
+  padding: '10px 3px',
+  border: `1px solid ${COLORS.schemes.outlineVariant}`,
+  width: '93vw',
+  margin: '0 auto',
+  borderRadius: '10px',
+  marginBottom: '27px',
+};
+
+export const bagTitle: SxProps<Theme> = {
+  fontFamily: FONT.family,
+  fontSize: FONT.fontSize.mediumPlus,
+  fontWeight: FONT.fontWeight.large,
+  color: COLORS.schemes.secondary,
+  marginLeft: '10px',
+};
+
+export const bagDetails: SxProps<Theme> = {
+  fontFamily: FONT.family,
+  fontSize: FONT.fontSize.medium,
+  fontWeight: FONT.fontWeight.medium,
+  color: COLORS.schemes.outline,
+  marginLeft: '10px',
+};
+
+export const weightLabel: SxProps<Theme> = {
+  paddingLeft: '10px',
+  fontFamily: FONT.family,
+  fontSize: FONT.fontSize.medium,
+  letterSpacing: '0.2px',
+  color: COLORS.schemes.secondary,
+  marginRight: '5px',
+};
+
+export const inputField: SxProps<Theme> = {
+  width: '60px',
+  height: '32px',
+  padding: '1px',
+
+  '.css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+    padding: '4px',
+  },
+};
+
+export const importantBlock: SxProps<Theme> = {
+  height: '175px',
+  padding: '10px 3px',
+  border: `1px solid ${COLORS.schemes.outlineVariant}`,
+  width: '93vw',
+  margin: '0 auto',
+  borderRadius: '10px',
+  marginBottom: '27px',
+};
+
+export const importantTitle: SxProps<Theme> = {
+  fontFamily: FONT.family,
+  fontSize: FONT.fontSize.mediumPlus,
+  marginLeft: '10px',
+};
+
+export const importantText: SxProps<Theme> = {
+  paddingLeft: '10px',
+  fontFamily: FONT.family,
+  fontSize: FONT.fontSize.medium,
+  letterSpacing: '0.2px',
+  color: COLORS.schemes.secondary,
+};
