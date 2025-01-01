b08ac307d34e94331cd654d5c619b5db4e6ba54d
Parle648 <sashahoumi@gmail.com>
2025-01-01 11:05:58 +0200

    + Create: conditional rendering for nav btns

--- src/constants/numbers.ts
+++ src/constants/numbers.ts
@@ -1,3 +1,4 @@
 export const ID_PREFIX_LENGTH = 5;
 export const PREVIOUS_PAGE = -1;
 export const MAX_WEIGHT = 40;
+export const ROUTE_PAGE_ID = 3;
--- src/pages/OrderDetails/components/BottomNavigationBar/index.tsx
+++ src/pages/OrderDetails/components/BottomNavigationBar/index.tsx
@@ -4,32 +4,40 @@ import { t } from 'i18next';
 
 import ArrowLeft from '@/assets/icons/arrow-left.svg';
 import Button from '@/components/Button';
+import { ROUTE_PAGE_ID } from '@/constants/numbers';
 import {
   BottomNavigation,
   IconWrapper,
   navigateStyles,
   RightArrow,
 } from '@/pages/OrderDetails/styles';
+import { useAppSelector } from '@/redux/hooks';
 
 interface BottomBarProps {
   openNavigateModal: () => void;
 }
 
 const BottomNavigationBar: FC<BottomBarProps> = ({ openNavigateModal }) => {
+  const { pageSelected: pageId } = useAppSelector((store) => store.pages);
+
   return (
     <BottomNavigation>
-      <IconWrapper>
-        <img src={ArrowLeft} alt={t('orderDetails.prev')} />
-      </IconWrapper>
+      {pageId === ROUTE_PAGE_ID && (
+        <IconWrapper>
+          <img src={ArrowLeft} alt={t('orderDetails.prev')} />
+        </IconWrapper>
+      )}
       <Button
         label={t('orderDetails.navigate')}
         variant='outlined'
         sx={navigateStyles}
         onClick={openNavigateModal}
       />
-      <IconWrapper>
-        <RightArrow src={ArrowLeft} alt={t('orderDetails.next')} />
-      </IconWrapper>
+      {pageId === ROUTE_PAGE_ID && (
+        <IconWrapper>
+          <RightArrow src={ArrowLeft} alt={t('orderDetails.next')} />
+        </IconWrapper>
+      )}
     </BottomNavigation>
   );
 };
